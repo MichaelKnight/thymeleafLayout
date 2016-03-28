c89d0612d37644337a65cc620ec2cfe20fc3b5f0
Francisco Javier Martinez Marin <franciscojaviermartinezmarin@gmail.com>
2016-03-28 14:57:37 +0800

    Layout+content interaction

---
+++ src/main/java/com/thymeleaflayout/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.thymeleaflayout;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by javier on 25/03/16.
+ */
+@Controller
+public class MvcConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/billing").setViewName("billing");
+        registry.addViewController("/hello").setViewName("hello");
+    }
+}
